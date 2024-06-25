29211a25f75d66426fa509cf46fe804327985c38
schluis <schluis@posteo.de>
2024-06-25 17:24:05 +0200

    add error handling

--- fetcher.py
+++ fetcher.py
@@ -8,6 +8,8 @@ import toml
 
 config = toml.load('config.toml')
 telegram_ids = config['telegram']['ids']
+telegram_admin_ids = config['telegram']['admin_ids']
+
 date = config['query_settings']['date']
 hut_id = config['query_settings']['hut_id']
 
@@ -17,33 +19,59 @@ room_types = {'7': 'Matratzenlager', '8': 'Mehrbettzimmer', '9': 'Zweierzimmer'}
 hut_link = f'https://www.alpsonline.org/reservation/calendar?hut_id={hut_id}&header=no'
 query_link = f'https://www.alpsonline.org/reservation/selectDate?date={date}'
 
+counter = 0
+error_counter = 0
 while True:
-    session = requests.Session()
-    session.get(hut_link)
+    try:
+        while True:
+            session = requests.Session()
+            session.get(hut_link)
 
-    request_cookies = session.cookies.get_dict()
-    headers = {'User-Agent': 'Mozilla/5.0'}
+            request_cookies = session.cookies.get_dict()
+            headers = {'User-Agent': 'Mozilla/5.0'}
 
-    raw_reservations = requests.get(query_link, cookies=request_cookies, headers=headers).text
-    parsed_reservations = json.loads(raw_reservations)
+            raw_reservations = requests.get(query_link, cookies=request_cookies, headers=headers).text
+            parsed_reservations = json.loads(raw_reservations)
 
-    reservations_on_selected_date = parsed_reservations['0']
+            reservations_on_selected_date = parsed_reservations['0']
 
-    total_number_of_free_rooms = 0
-    message = ''
-    for room_type in reservations_on_selected_date:
-        room_type_name = room_types[str(room_type['bedCategoryId'])]
-        free_rooms = room_type['freeRoom']
-        total_number_of_free_rooms += free_rooms
-        total_rooms = room_type['totalRoom']
-        reserved_rooms_ratio = room_type['reservedRoomsRatio']
-        message += f'{room_type_name} ({free_rooms}/{total_rooms}) - {reserved_rooms_ratio * 100:.2f}% reserved\n'
+            total_number_of_free_rooms = 0
+            message = ''
+            for room_type in reservations_on_selected_date:
+                room_type_name = room_types[str(room_type['bedCategoryId'])]
+                free_rooms = room_type['freeRoom']
+                total_number_of_free_rooms += free_rooms
+                total_rooms = room_type['totalRoom']
+                reserved_rooms_ratio = room_type['reservedRoomsRatio']
+                message += f'{room_type_name} ({free_rooms}/{total_rooms}) - {reserved_rooms_ratio * 100:.2f}% reserved\n'
 
-    if total_number_of_free_rooms > 0:
-        message += f"\nThere are {total_number_of_free_rooms} free rooms available at the hut on {date}!\n\n"
-        for telegram_id in telegram_ids:
-            asyncio.run(send_message(message, telegram_id))
+            if total_number_of_free_rooms > 0:
+                message += f"\nThere are {total_number_of_free_rooms} free rooms available at the hut on {date}!\n\n"
+                for telegram_id in telegram_ids:
+                    asyncio.run(send_message(message, telegram_id))
 
-    print(message)
+            print(message)
 
-    time.sleep(60)
+            if counter >= 30:
+                for telegram_id in telegram_admin_ids:
+                    asyncio.run(send_message(message, telegram_id))
+
+                counter = 0
+
+            time.sleep(60)
+            counter += 1
+
+    except Exception as e:
+        print(e)
+        
+        error_counter += 1 
+        if error_counter <=10:
+            for telegram_id in telegram_admin_ids:
+                asyncio.run(send_message(f"An error occurred: {e}\nExecution resumes in 120 seconds!", telegram_id))
+            
+            time.sleep(120)
+
+        else:  
+            for telegram_id in telegram_admin_ids:
+                asyncio.run(send_message(f"An error occurred: {e}\nExecution halted!", telegram_id))
+            raise e
