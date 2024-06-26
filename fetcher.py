0a11d9768bd7f931acfce02776e17d8f6c4ba797
schluis <luis.scheuch@posteo.de>
2024-06-26 09:21:59 +0200

    Support multiple days, formatting

--- fetcher.py
+++ fetcher.py
@@ -1,30 +1,32 @@
+import asyncio
+import json
+import time
+from datetime import datetime, timedelta
+
+import requests
+import toml
+
 from telegram_notifier import send_message
 
-import json 
-import requests
-import asyncio
-import time
-import toml
-from datetime import datetime
+config = toml.load("config.toml")
+telegram_ids = config["telegram"]["ids"]
+telegram_admin_ids = config["telegram"]["admin_ids"]
 
-config = toml.load('config.toml')
-telegram_ids = config['telegram']['ids']
-telegram_admin_ids = config['telegram']['admin_ids']
+date = config["query_settings"]["date"]
+days = config["query_settings"]["days"]
+hut_id = config["query_settings"]["hut_id"]
 
-date = config['query_settings']['date']
-hut_id = config['query_settings']['hut_id']
-
-room_types = {'7': 'Matratzenlager', '8': 'Mehrbettzimmer', '9': 'Zweierzimmer'}
+room_types = {"7": "Matratzenlager", "8": "Mehrbettzimmer", "9": "Zweierzimmer"}
 
 
-hut_link = f'https://www.alpsonline.org/reservation/calendar?hut_id={hut_id}&header=no'
-query_link = f'https://www.alpsonline.org/reservation/selectDate?date={date}'
+hut_link = f"https://www.alpsonline.org/reservation/calendar?hut_id={hut_id}&header=no"
+query_link = f"https://www.alpsonline.org/reservation/selectDate?date={date}"
 
 error_counter = 0
 last_sent_message = ""
 
 for telegram_id in telegram_admin_ids:
-    asyncio.run(send_message("The fetcher has been started!", telegram_id))
+    asyncio.run(send_message("Service up and running...", telegram_id))
 
 while True:
     try:
@@ -33,52 +35,72 @@ while True:
             session.get(hut_link)
 
             request_cookies = session.cookies.get_dict()
-            headers = {'User-Agent': 'Mozilla/5.0'}
+            headers = {"User-Agent": "Mozilla/5.0"}
 
-            raw_reservations = requests.get(query_link, cookies=request_cookies, headers=headers).text
+            raw_reservations = requests.get(
+                query_link, cookies=request_cookies, headers=headers
+            ).text
             parsed_reservations = json.loads(raw_reservations)
 
-            reservations_on_selected_date = parsed_reservations['0']
+            message = ""
+            for day in range(days):
+                reservations_on_selected_date = parsed_reservations[str(day)]
 
-            total_number_of_free_rooms = 0
-            message = ''
-            for room_type in reservations_on_selected_date:
-                room_type_name = room_types[str(room_type['bedCategoryId'])]
-                free_rooms = room_type['freeRoom']
-                total_number_of_free_rooms += free_rooms
-                total_rooms = room_type['totalRoom']
-                reserved_rooms_ratio = room_type['reservedRoomsRatio']
-                message += f'{room_type_name} ({free_rooms}/{total_rooms}) - {reserved_rooms_ratio * 100:.2f}% reserved\n'
+                total_number_of_free_rooms = 0
+                selected_date = datetime.strptime(date, "%d.%m.%Y") + timedelta(
+                    days=day
+                )
+                message += (
+                    f"Reservations for {selected_date.date().strftime('%d.%m.%Y')}:\n"
+                )
 
-            if total_number_of_free_rooms > 0:
-                message += f"\nThere are {total_number_of_free_rooms} free rooms available at the hut on {date}!\n\n"
+                for room_type in reservations_on_selected_date:
+                    room_type_name = room_types[str(room_type["bedCategoryId"])]
+                    free_rooms = room_type["freeRoom"]
+                    total_number_of_free_rooms += free_rooms
+                    total_rooms = room_type["totalRoom"]
+                    reserved_rooms_ratio = room_type["reservedRoomsRatio"]
+                    message += f"{room_type_name} ({free_rooms}/{total_rooms}) - {reserved_rooms_ratio * 100:.2f}% reserved\n"
+
+                if total_number_of_free_rooms > 0:
+                    message += f"\nThere are {total_number_of_free_rooms} free rooms available at the hut!\n\n\n"
 
             if message != last_sent_message:
                 print(message)
                 for telegram_id in telegram_ids:
                     asyncio.run(send_message(message, telegram_id))
-                
+
                 last_sent_message = message
 
-            if datetime.now().hour == 9 and datetime.now().minute == 0:
+            if datetime.now().hour == 7 and datetime.now().minute == 0:
                 error_counter = 0
                 for telegram_id in telegram_admin_ids:
-                    asyncio.run(send_message("Service up and running...", telegram_id))
+                    asyncio.run(
+                        send_message("Service still up and running...", telegram_id)
+                    )
 
-            sleeptime = 120 - datetime.now().second
-            time.sleep(sleeptime)
+            time.sleep(120)
 
     except Exception as e:
         print(e)
-        
-        error_counter += 1 
-        if error_counter <=20:
+
+        error_counter += 1
+        if error_counter <= 20:
             for telegram_id in telegram_admin_ids:
-                asyncio.run(send_message(f"An error occurred: {e}\nExecution resumes in 120 seconds!", telegram_id))
-            
+                asyncio.run(
+                    send_message(
+                        f"An error occurred: {e}\nExecution resumes in 120 seconds!",
+                        telegram_id,
+                    )
+                )
+
             time.sleep(120)
 
-        else:  
+        else:
             for telegram_id in telegram_admin_ids:
-                asyncio.run(send_message(f"An error occurred: {e}\nExecution halted!", telegram_id))
+                asyncio.run(
+                    send_message(
+                        f"An error occurred: {e}\nExecution halted!", telegram_id
+                    )
+                )
             raise e
